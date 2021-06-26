89de487dd9ec3067df7143e9ccf44d0dc497bfdd
DaN1zera <daniielreis@live.com>
2021-06-26 13:59:19 -0300

    Adicionado customizações vizuais e mais alternativas para fazer novas operações

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -1,34 +1,40 @@
+import os
+
+import colorama
+from colorama import Fore
+
 import calculadora_modulos
 
-while True:
-    calculadora = calculadora_modulos.Calculadora()
-    chamada = int(
-        input(
-            'Soma(0)'
-            '\nSubtração(1)'
-            '\nMultiplicação(2)'
-            '\nDivisão(3)'
-            '\nBhaskara(4)'
-            '\nEscolha a operação que deseja fazer: '
-        )
-    )
-    while chamada > 4:
-        print('\nVoce escolheu uma opção que não existe')
-        chamada = int(input('Tente novamente: '))
+calculadora = calculadora_modulos.Calculadora()
 
-    if chamada == 0:
+
+os.system('cls')
+
+while True:
+    print(Fore.BLUE + '=' * 34, Fore.BLUE + '\n========CALCULADORA PYTHON========')
+    print(Fore.BLUE + '=' * 34)
+    print()
+    chamada = input(Fore.WHITE + 'Soma...........: 0' '\nSubtração......: 1' '\nMultiplicação .: 2' '\nDivisão........: 3' '\nBhaskara.......: 4' '\nEscolha a operação que deseja fazer: ')
+    while chamada not in ('0', '1', '2', '3', '4'):
+        print('Voce escolheu uma opção que não existe')
+        chamada = input('Tente novamente: ')
+    os.system('cls')
+
+    if chamada == '0':
         print(calculadora.soma())
-    elif chamada == 1:
+    elif chamada == '1':
         print(calculadora.subtracao())
-    elif chamada == 2:
+    elif chamada == '2':
         print(calculadora.multiplicacao())
-    elif chamada == 3:
+    elif chamada == '3':
         print(calculadora.divisao())
-    elif chamada == 4:
+    elif chamada == '4':
         print(calculadora.bhaskara())
 
-    continua = input('Deseja fazer uma nova operação? s/n: ')
-    while continua != 's' and continua != 'n':
-        continua = input('Responda com s ou n? s/n: ')
-    if continua == 'n':
+    continua = input(Fore.WHITE + 'Deseja fazer uma nova operação?: ').lower()
+    while continua not in ('s', 'n', 'sim', 'nao', 'não'):
+        continua = input('Responda com Sim ou Não: ')
+    if continua == 'n' or continua == 'nao' or continua == 'não':
+        os.system('cls')
         break
+    os.system('cls')
--- Calculadora/calculadora_modulos.py
+++ Calculadora/calculadora_modulos.py
@@ -1,42 +1,56 @@
+import colorama
+from colorama import Fore
+
+branco = Fore.WHITE
+vermelho = Fore.RED
+azul = Fore.BLUE
+verde = Fore.GREEN
+
+
 class Calculadora:
     def bhaskara(self):
-        a = float(input('insira o valor de a: '))
-        b = float(input('insira o valor de b: '))
-        c = float(input('insira o valor de c: '))
+        print(azul + '========Bhaskara========')
+        a = float(input(f'{branco}insira o valor de a: {vermelho}'))
+        b = float(input(f'{branco}insira o valor de b: {vermelho}'))
+        c = float(input(f'{branco}insira o valor de c: {vermelho}'))
         delta = (b * b) - 4 * (a * c)
         x1 = (-(b) + float(delta) ** 0.5) / (2 * a)
         x2 = (-(b) - float(delta) ** 0.5) / (2 * a)
-        print(f'Valor de delta: {delta}')
-        print(f'Valor de x1: {x1}')
-        print(f'Valor de x2: {x2}')
-        return input('Aperte "enter" para continuar ')
+        print(f'{branco}Valor de delta:{verde} {delta}')
+        print(f'{branco}Valor de x1:{verde} {x1}')
+        print(f'{branco}Valor de x2:{verde} {x2}')
+        return input(branco + 'Aperte "enter" para continuar ')
 
     def soma(self):
-        a = float(input('insira o valor de a: '))
-        b = float(input('insira o valor de b: '))
+        print(azul + '========Soma========')
+        a = float(input(f'{branco}insira o valor de a: {vermelho}'))
+        b = float(input(f'{branco}insira o valor de b: {vermelho}'))
         soma = a + b
-        print(f'Resultado: {soma}')
-        return input('Aperte "enter" para continuar ')
+        print(f'{branco}Resultado:{verde} {soma}')
+        return input(branco + 'Aperte "enter" para continuar ')
 
     def subtracao(self):
-        a = float(input('insira o valor de a: '))
-        b = float(input('insira o valor de b: '))
+        print(azul + '========Subtração========')
+        a = float(input(f'{branco}insira o valor de a: {vermelho}'))
+        b = float(input(f'{branco}insira o valor de b: {vermelho}'))
         subtracao = a - b
-        print(f'Resultado: {subtracao}')
-        return input('Aperte "enter" para continuar ')
+        print(f'{branco}Resultado:{verde} {subtracao}')
+        return input(branco + 'Aperte "enter" para continuar ')
 
     def multiplicacao(self):
-        a = float(input('insira o valor de a: '))
-        b = float(input('insira o valor de b: '))
+        print(azul + '========Multiplicação========')
+        a = float(input(f'{branco}insira o valor de a: {vermelho}'))
+        b = float(input(f'{branco}insira o valor de b: {vermelho}'))
         multiplicacao = a * b
-        print(f'Resultado: {multiplicacao}')
-        return input('Aperte "enter" para continuar ')
+        print(f'{branco}Resultado:{verde} {multiplicacao}')
+        return input(branco + 'Aperte "enter" para continuar ')
 
     def divisao(self):
-        a = float(input('insira o valor de a: '))
-        b = float(input('insira o valor de b: '))
+        print(azul + '========Divisão========')
+        a = float(input(f'{branco}insira o valor de a: {vermelho}'))
+        b = float(input(f'{branco}insira o valor de b: {vermelho}'))
         divisao = a / b
         resto = a % b
-        print(f'Resultado: {divisao}')
-        print(f'Resto: {resto}')
-        return input('Aperte "enter" para continuar ')
+        print(f'{branco}Resultado:{verde} {divisao}')
+        print(f'{branco}Resto:{verde} {resto}')
+        return input(branco + 'Aperte "enter" para continuar ')
